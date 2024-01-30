8d17fa535c694b796a4bc1e636d04e7302738acd
Sam Hoggatt <samuelchoggatt@gmail.com>
2024-01-29 22:20:10 -0600

    Add route to go to event overviews.

--- client/src/app/shared/components/event-overview.tsx
+++ client/src/app/shared/components/event-overview.tsx
@@ -6,12 +6,12 @@ import UserTile from "./user-tile";
 // Probably should only show a max of like 10 RSVP'd users
 function EventOverview(props: { event: IEvent }) {
   return (
-    <div className="justify-center text-center">
-      <h1 className="text-5xl mb-3">{props.event.name}</h1>
+    <div className="justify-center text-center w-1/2">
+      <h1 className="text-4xl mb-3">{props.event.name}</h1>
       <p className="mb-3">{props.event.description}</p>
       <CountdownTimer targetDate={props.event.date} />
       <button
-        className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+        className="inline-flex items-center m-4 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         onClick={() => console.log("something should happen here...")}
       >
         Join Event
