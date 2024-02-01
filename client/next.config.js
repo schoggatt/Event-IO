e4342a14a320b9c5ba622cc9a23dbd7fa66c90bd
Sam Hoggatt <samuelchoggatt@gmail.com>
2024-02-01 14:50:06 -0600

    Fix configuration issues.

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {domains: ['lh3.googleusercontent.com']}
+    images: {domains: ['lh3.googleusercontent.com']},
 }
 
 module.exports = nextConfig
